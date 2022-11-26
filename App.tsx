7fe1f93ccc21bcddca52cda0a81a5de5e75de6d0
pedro10r <pedrosantos.ti.web@gmail.com>
2022-11-26 11:54:00 -0300

    feat: add default theme in Native Base

--- App.tsx
+++ App.tsx
@@ -7,13 +7,14 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 
+import { THEME } from './src/theme/index';
 import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
